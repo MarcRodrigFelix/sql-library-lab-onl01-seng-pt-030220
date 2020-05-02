c8b37258479abea771eb8794a7dffe74f220a3cf
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-05-01 23:35:11 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,44 @@
+INSERT INTO series (title, author_id, subgenre_id)
+VALUES ("Fantasy", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id)
+VALUES ("Comedy", 1, 2);
+
+INSERT INTO books (title, year, series_id) VALUES ("Gray Matter", 1998, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Black Matter", 1999, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Grey Matter", 2000, 3);
+INSERT INTO books (title, year, series_id) VALUES ("White Matter", 2001, 4);
+INSERT INTO books (title, year, series_id) VALUES ("Green Matter", 2000, 5);
+INSERT INTO books (title, year, series_id) VALUES ("Blue Matter", 2001, 6);
+
+
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 1);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Mark", "Alien", "Freedom", 2);
+
+INSERT INTO subgenres (name) VALUES ("Mystery");
+INSERT INTO subgenres (name) VALUES ("Drama");
+
+INSERT INTO authors (name) VALUES ("Mark");
+INSERT INTO authors (name) VALUES ("Marcus");
+
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,46 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year
+   FROM Books
+   WHERE Books.series_id = 1
+   ORDER BY(Books.year);"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT Characters.name, Characters.motto
+   FROM Characters
+   ORDER BY(LENGTH(Characters.motto)) DESC LIMIT 1;"  
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT Characters.species, COUNT(Characters.species)
+   FROM Characters
+   GROUP BY Characters.species
+   ORDER BY COUNT(Characters.species) DESC
+   LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT Authors.name, Subgenres.name
+  FROM Subgenres
+  INNER JOIN Authors
+  ON Subgenres.id = Authors.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT Series.title
+   FROM Series
+   INNER JOIN Characters
+   GROUP BY Characters.species
+   HAVING Characters.species = 'human';"
+  #  WHERE Characters.species = 'human' LIMIT 1;" #<==== THIS WORKS TOO.
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT Characters.name, COUNT(name)
+   FROM Character_books
+   INNER JOIN Characters
+   ON Characters.id = Character_books.character_id
+   GROUP BY Character_books.character_id
+   ORDER BY(COUNT(name)) DESC;"
 end
